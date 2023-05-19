a768a2f439a378bf4cb8199cd17685aa0c7262fd
User <User@DESKTOP-ODGHGHR>
2023-05-19 18:35:48 -0300

    Criação do projeto

---
+++ projeto1/src/cursojava/executavel/PrimeiraClassjava.java
@@ -0,0 +1,16 @@
+package cursojava.executavel;
+
+public class PrimeiraClassjava {
+	
+        /*Main é um metodo auto executavel em Java*/
+	
+	public static void main(String[] args) {
+		
+		 System.out.print("meu primeiro projeto em java\n");
+		 System.out.print("vou programar meu primeiro projeto em java\n"); 
+		 System.out.print("estou começando meu primeiro projeto em java\n"); 
+		 System.out.print("estou conseguindo programar meu primeiro projeto em java\n"); 
+				 
+				 
+	}
+}
