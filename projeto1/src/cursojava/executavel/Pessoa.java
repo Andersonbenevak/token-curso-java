26dd42bc1a6a8de69c1c3f156896188bd1f01823
User <User@DESKTOP-ODGHGHR>
2023-05-20 19:35:55 -0300

    adicionando idade no projeto

--- projeto1/src/cursojava/executavel/Pessoa.java
+++ projeto1/src/cursojava/executavel/Pessoa.java
@@ -2,4 +2,40 @@ package cursojava.executavel;
 
 public class Pessoa {
 
+	
+	private String nome;
+        	
+    private String sobrenome;
+    
+    private int idade;
+    
+    public int getIdade() {
+    	return idade;
+    }
+    
+    public String getnome() {
+    	return nome;
+    }
+    
+    
+    
+    
+    
+    public void setNome(String nome) {
+		this.nome = nome;
+	}
+    
+    public String getSobrenome() {
+	   return sobrenome;
+	   }
+    
 }
+
+
+
+
+
+
+
+
+
